b3324730272c4922442cfe995a85198dd4b97fef
Senthamizhselvij <109455436+Senthamizhselvij@users.noreply.github.com>
2022-07-20 22:13:09 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,95 @@
+
+package constructor;
+
+public class Employee {
+
+	int empId;
+
+	String empName;
+
+	String department;
+
+	float salary;
+
+	
+
+	//default constructor
+
+	public Employee() {
+
+		empId=5;
+
+		empName="Emp001";
+
+		department="finance";
+
+		salary=45000;
+
+	}
+
+	
+
+	//parameterized constructor
+
+	public Employee(int empId,String empName,String department,float salary) {
+
+		this.empId=empId;
+
+		this.empName=empName;
+
+		this.department=department;
+
+		this.salary=salary;
+
+	}
+
+	
+
+	public void display() {
+
+		System.out.println("Id: "+empId);
+
+		System.out.println("Name: "+empName);
+
+		System.out.println("Department: "+department);
+
+		System.out.println("Salary: "+salary);
+
+		System.out.println();
+
+		
+
+	}
+
+	
+
+	public static void main(String[] args) {
+
+		
+
+		Employee e= new Employee();
+
+		Employee e1= new Employee(18, "tamil", "Training", 464500); 
+
+
+
+		//calling default constructor
+
+		e.display();
+
+		//parameterized constructor
+
+		e1.display();
+
+		
+
+		 
+
+	
+
+	}
+
+
+
+	
+}
