6f25ce429739eb2af626dea55e00282e8417a272
senthamizhselvi <senthamizhselvi12112000@mail.com>
2022-09-14 13:41:55 +0530

    project

---
+++ JUnit project/UserAuthentication/src/main/java/com/simplilearn/demo/AuthenticationUser.java
@@ -0,0 +1,16 @@
+package com.simplilearn.demo;
+
+public class AuthenticationUser {
+	public String username()
+	{
+		String email = "tamil@gmail.com";
+		return email;
+	}
+	
+	public String paswd()
+	{
+		String password = "tamil";
+		return password;
+	}
+
+}
